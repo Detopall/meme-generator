392e31a638209dab9b41d06a45882079e70ce4b1
denis.topallaj <denis.topallaj@student.howest.be>
2022-09-08 10:40:47 +0200

    added some styling and the images.

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,10 +9,20 @@ const POST_MEMES_URL = "https://api.imgflip.com/caption_image";
 async function init(){
 	const arrayMemes = await fetchInfo();
 	console.log(arrayMemes);
+	displayImagesOfMemes(arrayMemes);
 }
 
 async function fetchInfo(){
 	const fetchMemes = await fetch(GET_MEMES_URL);
 	const res = await fetchMemes.json();
 	return res.data.memes;
+}
+
+function displayImagesOfMemes(arrayMemes){
+	const chooseMeme = document.querySelector("#choose-meme");
+	arrayMemes.forEach(meme => {
+		let html = 
+		`<img alt="${meme.name}" title="${meme.name}" src="${meme.url}">`;
+		chooseMeme.insertAdjacentHTML("beforeend", html);
+	});
 }
